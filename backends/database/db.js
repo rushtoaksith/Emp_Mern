f977d4b9a58b6e0cd88205190591726c166a97ad
Intellipaat <110481066+rushtoaksith@users.noreply.github.com>
2024-07-12 17:33:41 +0530

    Update db.js

--- backends/database/db.js
+++ backends/database/db.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb://localhost:27017/oxy_book?directConnection=true&tls=false&readPreference=primary";
-const mongoURI="mongodb+srv://somu:sg334s22@cluster0.mvf57zm.mongodb.net/riva_backend?retryWrites=true&w=majority"
-const connectToMongoose=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("Connected to Mongoose");
+
+const mongoURI = "mongodb://mongodb-service:27017/riva_backend"; // MongoDB service name and port
+
+const connectToMongoose = () => {
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        auth: {
+            authSource: 'admin' // MongoDB admin database for authentication
+        },
+        user: process.env.MONGO_INITDB_ROOT_USERNAME, // MongoDB username from environment variable
+        pass: process.env.MONGO_INITDB_ROOT_PASSWORD // MongoDB password from environment variable
     })
-}
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+};
+
 module.exports = connectToMongoose;
